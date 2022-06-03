9b6cc4e1b1e62637905814ae86060b20d74bb4d1
Rukundo fidele <rukufid@gmail.com>
2022-06-03 13:05:45 -0700

    This is a new commit for what I originally planned to be an amendmend

---
+++ src/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'Nike-slim-shirt',
+      category: 'shirts',
+      image: '/images/d1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike Slim pant',
+      slug: 'Nike-slim-pant',
+      category: 'pants',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality pant',
+    },
+    {
+      name: 'Adidas fit pant',
+      slug: 'Adidas-fit-pant',
+      category: 'pants',
+      image: '/images/p2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality pant',
+    },
+  ],
+};
+
+export default data;
